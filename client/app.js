87ce3dc369a74317edb54e318feb7263715b03b1
rayqi <qi.raymond@gmail.com>
2018-11-04 16:30:01 -0500

    set up babel, webpack and react

---
+++ client/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render() {
+        return (
+            <h1>HEY YA IM WORKING without DEfault</h1>
+        )
+    }
+}
+
